83bb86e123ac4c9a6f2e0b0810c360054c3fb048
Anderson Jorge <anderson.jorge33@gmail.com>
2021-07-12 16:57:58 -0300

    inicio do alurakut

---
+++ src/components/Box/index.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px; 
+
+  padding: 16px;
+
+  margin-bottom: 10px;
+  .box.link{
+    font-size: 14px;
+    color: #2E7BB4;
+    text-decoration: none;
+    font-weight: 800;
+  }
+  .title {
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+  .subTitle {
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 20;
+  }
+
+  .smallTitle {
+    margin-bottom: 20px;
+    font-size: 16px;
+    font-weight: 700;
+    color: #333333;
+  }
+`;
+
+export default Box;
